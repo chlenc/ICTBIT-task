2143a795e2d5b5b0816bc36e70360c5a49ca7ab6
Mendel Bakaleynik <mendelbak@gmail.com>
2021-05-31 12:39:08 +0300

    Removed some starter code.

--- updateCurrentUser.js
+++ updateCurrentUser.js
@@ -1,13 +1,5 @@
 // updateCurrentUser = (currentUserID) => {
 //     try {
-//       const userSettings = await UserSchema.findOneAndUpdate(
-//         // Fill in options here
-//       );
-
-//         // Fill in options here
-//     } catch (err) {
-//         // Fill in options here
-//     }
 //   };
 
 // Please comment out all your code when you are finished.
